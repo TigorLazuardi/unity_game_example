22f98efb5a1e50c4716dd0cb166637a12e93083b
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2021-01-09 17:18:57 +0700

    I think it's done

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    void Start()
+    {
+        guess = UnityEngine.Random.Range(min, max + 1);
+        setView(guess);
+    }
+
+    public void onPressHigher()
+    {
+        NextGuess(true);
+        setView(guess);
+    }
+
+    public void onPressLower()
+    {
+        NextGuess(false);
+        setView(guess);
+    }
+
+    void NextGuess(bool goUp)
+    {
+        if (goUp)
+        {
+            min = guess + 1;
+        }
+        else
+        {
+            max = guess - 1;
+        }
+        guess = UnityEngine.Random.Range(min, max + 1);
+    }
+
+    void setView(int value)
+    {
+        guessText.text = value.ToString();
+    }
+
+    void restartGame()
+    {
+        min = 1;
+        max = 1000;
+        guess = 500;
+    }
+}
